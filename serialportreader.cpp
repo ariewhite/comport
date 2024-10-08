49f5154c87fdfde4549920b444bee478b551b1b4
ariewhite <arie.white@bk.ru>
2024-10-08 19:19:34 +0500

    forms

---
+++ serialportreader.cpp
@@ -0,0 +1,6 @@
+#include "serialportreader.h"
+
+SerialPortReader::SerialPortReader()
+{
+
+}
---
+++ serialportreader.h
@@ -0,0 +1,11 @@
+#ifndef SERIALPORTREADER_H
+#define SERIALPORTREADER_H
+
+
+class SerialPortReader
+{
+public:
+    SerialPortReader();
+};
+
+#endif // SERIALPORTREADER_H
